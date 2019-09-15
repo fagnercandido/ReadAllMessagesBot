339da9fc19a7bf3dd1ecb04adfe2e2d4fb1bd73e
Fagner Candido <fagner.candido@globalweb.com.br>
2019-09-15 11:00:23 +0100

    refactored implementation

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 
 import os
 import telebot
+from telebot import types
 import urllib
 import json
 
@@ -11,5 +12,9 @@ botRead = telebot.TeleBot(os.environ["BOT_READ_ALL_MESSAGES"])
 def send_start_message(message):
     botRead.reply_to(message, "Olá, eu sou o Bot 'Read All Messages' \n"
                           "Diariamente irei enviar as mensagens não lidas para seu e-mail")
-                    
+
+@botRead.message_handler(func=lambda message: True)
+def get_handle_chats(message):
+    botRead.reply_to(message, message.chat.id)
+
 botRead.polling()
